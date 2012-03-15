07470a9b3a35273de79c96cf93beda06efa7dc89
Srikant Patnaik <usp71084@gmail.com>
2012-03-15 10:00:43 +0530

    added commits & readme

---
+++ README.rst
@@ -0,0 +1,28 @@
+Introduction
+============
+This script will check periodically the status of a git repository and add/rm 
+files based on any change made by user in that directory. It is an
+attempt to use git as incremental backup tool. User can select a restore point
+based on periodic commits.
+
+
+Packages required
+-----------------
+
+ #. git 
+
+ #. python
+
+
+Usage
+-----
+Just run the script by 
+:: 
+ 
+    python backup.py
+
+
+License
+-------
+GNU GPLV3
+
--- backup.py
+++ backup.py
@@ -1,26 +1,26 @@
 import os,time
 from subprocess import Popen, PIPE
 eachfile = ''
+
 if not os.path.exists('.git'):
     os.system('git init')
 
 while True:
     bashout = Popen('git status',shell=True, stdout=PIPE).stdout.readlines()
     
-    """captures git status and next 'if' statement checks status of git commit """
+    """captures git status from stdout, 'if' statement checks line2 from 
+    stdout with standard commit message """
 
     if not bashout[1].strip() == 'nothing to commit (working directory clean)':
         userDeleted = Popen('git ls-files --deleted',shell=True, stdout=PIPE).stdout.readlines()
-        for i in userDeleted:
+        for i in userDeleted:                                #for more than one deleted files                   
             eachfile = i.strip()
-            rm = 'git rm ' + eachfile
- #           print eachfile
-            os.system(rm)                      # git rm files
-        os.system('git add .')   
-        commit = 'git commit -am ' + "'" + eachfile + "'"
-#        print commit
+            rm = 'git rm ' + eachfile                        #adding all deleted files to staging area
+            os.system(rm)             
+        os.system('git add .')                               #adding all new files 
+        commit = 'git commit -am ' + "'" + eachfile + "'"    #adding modified files
         os.system(commit)
-        time.sleep(5)
+        time.sleep(5)                                        #delay in seconds
     
     else:    
         os.system('git add .')
