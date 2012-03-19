6f524ce639f6734a1e1b5b84a641af2889d5fd0f
psachin <iclcoolster@gmail.com>
2012-03-19 19:07:25 +0530

    modified to do backups using basic 'cp' command

--- test1.py
+++ test1.py
@@ -1,15 +1,28 @@
 #!/usr/bin/python -tt
-# test script for android
+# test script
 
-
+import os,sys
 import time
 import android
 say = android.Android()
 
-while True:	
-	say.makeToast("what say ?")
-	print "pop-up"
-	time.sleep(5)
+def backupSync(srcDir,destDir):
+    # check for src dir
+    if not os.path.exists(srcDir):
+        #sys.exit("Source directory does not exist!")
+        say.makeToast("Src dir not exist!")
+    else:
+        # check if the destination path already exist, if not, create it.
+        if not os.path.exists(destDir):
+            os.mkdir(destDir) # make directory
+            print "Destination dir created: ", destDir
+        else:
+            rsync_command = 'cp -r ' + srcDir + ' ' + destDir
+            os.system(rsync_command)
+            print "Content successfully backed up"
+            say.makeToast("Content successfully backed up")
+            
+backupSync('/sdcard/src/','/sdcard/dest')
 
 	
 
