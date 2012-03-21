ee3b62324595c1229973e1b8541257e05fae6f7a
Srikant Patnaik <usp71084@gmail.com>
2012-03-21 10:26:17 +0530

    added basic gui android version

---
+++ sl4a/sl4a-backup-restore.py
@@ -0,0 +1,41 @@
+#name = droid.dialogGetInput("Enter the path")
+
+import os
+import time
+import android
+droid = android.Android()
+
+def backupSync(srcDir,destDir):
+    
+    
+    droid.dialogCreateAlert('Backup/Restore Tool',' Do you want to proceed')
+    droid.dialogSetPositiveButtonText('Yes')
+    droid.dialogSetNegativeButtonText('Not now')
+    droid.dialogShow()
+    yesNo = droid.dialogGetResponse().result
+
+    if yesNo["which"] == 'positive':
+        choice = ['backup','restore']
+        droid.dialogSetSingleChoiceItems(choice)
+        droid.dialogShow()
+        backupRestore = droid.dialogGetResponse().result
+        print backupRestore
+    else:
+        droid.makeToast("Good Bye!")
+
+    # check for src dir
+    if not os.path.exists(srcDir):
+        droid.makeToast("Src dir not exist!")
+    else:
+        # check if the destination path already exist, if not, create it.
+        if not os.path.exists(destDir):
+            os.mkdir(destDir) # make directory
+            print "Destination dir created: ", destDir
+        else:
+            rsync_command = 'cp -r ' + srcDir + ' ' + destDir
+            os.system(rsync_command)
+            print "Content successfully backed up"
+            droid.makeToast("Content successfully backed up")
+
+# call function
+backupSync('/sdcard/src/','/sdcard/dest')
